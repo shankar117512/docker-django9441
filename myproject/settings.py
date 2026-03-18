dda6996dfff14d4113d4e8b02ff4bc70710feb5b
shankar117512 <shankar117512@gmail.com>
2026-03-18 14:23:49 +0000

    Csrf adde

--- myproject/settings.py
+++ myproject/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-CSRF_TRUSTED_ORIGINS = ['']
+CSRF_TRUSTED_ORIGINS = ['https://docker-django9441-production.up.railway.app']
 
 
 # Quick-start development settings - unsuitable for production
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['docker-django9441-production.up.railway.app']
 
 
 # Application definition
